b364634a3599eeea1f91e444e2393c18c705589b
Gerremy <gfergus6@villanova.edu>
2023-10-27 13:19:45 -0400

    Initial Commit

---
+++ senior-project-crowd-new/senior_project_crowd_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  senior_project_crowd_newApp.swift
+//  senior-project-crowd-new
+//
+//  Created by Gerremy F on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct senior_project_crowd_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ senior-project-crowd-newUITests/senior_project_crowd_newUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  senior_project_crowd_newUITestsLaunchTests.swift
+//  senior-project-crowd-newUITests
+//
+//  Created by Gerremy F on 10/27/23.
+//
+
+import XCTest
+
+final class senior_project_crowd_newUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
