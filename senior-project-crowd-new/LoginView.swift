6c55e002dd2a6677c89e7e223213606d737dd564
Gerremy <gfergus6@villanova.edu>
2023-11-01 03:45:49 -0400

    Completed LoginView

--- senior-project-crowd-new/LoginView.swift
+++ senior-project-crowd-new/LoginView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     
     @State var isLoginMode = false
     @State var email = ""
@@ -26,14 +26,14 @@ struct ContentView: View {
                     }.pickerStyle(SegmentedPickerStyle())
                         //.padding()
                     
-                    if isLoginMode {
+                    if !isLoginMode {
                         
                         Button {
                             
                         } label: {
                             Image(systemName: "person.3.sequence.fill")
                                 .symbolRenderingMode(.hierarchical)
-                                .foregroundStyle(.cyan)
+                                .foregroundStyle(Color(.init(red: 0.45, green: 0.9, blue: 0.9, alpha: 0.8)))
                                 //.background(Color.white)
                                 .font(.system(size: 94))
                                 .padding()
@@ -41,36 +41,50 @@ struct ContentView: View {
                         
                     }
                     
-                    TextField("Email", text: $email)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                    SecureField("Password", text: $password)
-                        .autocapitalization(.none)
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                            .autocapitalization(.none)
+                    }
+                    .padding(11)
+                    .background(Color.white)
                     
                     //Text("Here is my account creation page")
                     
                     Button {
-                        
+                        handleAction()
                     } label: {
                         HStack {
                             Spacer()
-                            Text("Create Account")
+                            Text(isLoginMode ? "Log In" : "Create Account")
                                 .foregroundColor(.white)
                                 .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
                             Spacer()
-                        }.background(Color.cyan)
+                        }.background(Color(.init(red: 0.45, green: 0.9, blue: 0.9, alpha: 0.8)))
                     }
                     
                 }
                 .padding()
-                .background(Color(.init(white: 0, alpha: 0.05)))
+                .background(Color(.init(white: 0, alpha: 0.03)))
             }
             .navigationTitle(isLoginMode ? "Log In" : "Create Account")
-            .background(Color(.init(red: 0.2, green: 0.8, blue: 0.8, alpha: 0.4)))
+            //.background(Color(.init(red: 0.95, green: 0.7, blue: 0.65, alpha: 0.5)))
         }
     }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing credentials")
+        } else {
+            print("Register a new acount inside of Firebase Auth and then store image in Storage")
+        }
+    }
+    
 }
 
 #Preview {
-    ContentView()
+    LoginView()
 }
--- senior-project-crowd-new/senior_project_crowd_newApp.swift
+++ senior-project-crowd-new/senior_project_crowd_newApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct senior_project_crowd_newApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
